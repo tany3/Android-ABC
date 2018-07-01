9828d31bed4c451a54306aaf8a4adda71a43afd1
mtaniuchi <mtaniuchi@gmail.com>
2018-07-01 13:42:59 +0900

    modified: タイムゾーンを追加して表示する

--- 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/MainActivity.kt
+++ 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/MainActivity.kt
@@ -1,9 +1,12 @@
 package net.tiesfeed.android.sample.worldclock
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import java.util.*
 
@@ -30,6 +33,42 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, TimeZoneSelectActivity::class.java)
             // 遷移先画面からの結果を受け取りたい場合
             startActivityForResult(intent, 1)
+        }
+
+        // 世界時計のリストを表示する
+        showWorldClocks()
+    }
+
+    private fun showWorldClocks() {
+        // プリファレンスから、保存しているタイムゾーンを得る
+        val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val timeZones = pref.getStringSet("time_zone", setOf())
+
+        val list = findViewById<ListView>(R.id.clockList)
+        list.adapter = TimeZoneAdapter(this, timeZones.toTypedArray())
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1
+                && resultCode == Activity.RESULT_OK
+                && data != null) {
+            // 受け取ったデータから、タイムゾーンを得る
+            val timeZone = data.getStringExtra("timeZone")
+
+            // プリファレンスから、保存しているタイムゾーンを得る
+            val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+            val timeZones = pref.getStringSet("time_zone", mutableSetOf())
+
+            // 保存していたタイムゾーン一覧に追加
+            timeZones.add(timeZone)
+
+            // プリファレンスに保存する
+            pref.edit().putStringSet("time_zone", timeZones).apply()
+
+            // リストを再表示する
+            showWorldClocks()
         }
     }
 }
