61be426b8bd2e2eb9128e909e48137c11214d13c
mtaniuchi <mtaniuchi@gmail.com>
2018-07-01 13:33:54 +0900

    modified: タイムゾーンを表示する

--- 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/TimeZoneSelectActivity.kt
+++ 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/TimeZoneSelectActivity.kt
@@ -1,12 +1,45 @@
 package net.tiesfeed.android.sample.worldclock
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class TimeZoneSelectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // タイムゾーン選択画面のレイアウトを指定する
         setContentView(R.layout.activity_time_zone_select)
+
+        // 最初に「キャンセルされた」結果を返すようにしておくと、
+        // 戻るボタンをタップしたときなどに対応できる
+        setResult(Activity.RESULT_CANCELED)
+
+        // タイムゾーンリストをレイアウトから探す
+        val list = findViewById<ListView>(R.id.clockList)
+        // タイムゾーンリスト表示用のアダプターを作る
+        val adapter = TimeZoneAdapter(this)
+        // リストにアダプターをセットする
+        list.adapter = adapter
+
+        // リストタップ時の動作
+        // Kotlinでは、ラムダ式で使用しない引数を _ で省略できる
+        list.setOnItemClickListener { _, _, position, _ ->
+            // タップした場所のタイムゾーンをリストから得る
+            val timeZone = adapter.getItem(position)
+
+            // 遷移元の画面に返す結果
+            val result = Intent()
+            // ユーザーがタップしたタイムゾーンを設定する
+            result.putExtra("timeZone", timeZone)
+            // 「OK」の結果を返す
+            setResult(Activity.RESULT_OK, result)
+
+            // この画面を閉じる
+            finish()
+        }
     }
 }
