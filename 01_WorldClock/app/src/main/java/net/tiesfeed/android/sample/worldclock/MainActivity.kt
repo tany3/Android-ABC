362ac9acef10919ed422ec091287bc0749294d81
mtaniuchi <mtaniuchi@gmail.com>
2018-07-01 12:09:16 +0900

    modified: 時刻表示画面の実装 その1

--- 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/MainActivity.kt
+++ 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/MainActivity.kt
@@ -2,6 +2,8 @@ package net.tiesfeed.android.sample.worldclock
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,13 @@ class MainActivity : AppCompatActivity() {
 
         // 画面のレイアウトを指定
         setContentView(R.layout.activity_main)
+
+        // ユーザーのデフォルトタイムゾーンを取得する
+        var timeZone = TimeZone.getDefault()
+
+        // タイムゾーン名を表示する
+        val timeZoneView = findViewById<TextView>(R.id.timeZone)
+        // タイムゾーンを表示する
+        timeZoneView.text = timeZone.displayName
     }
 }
