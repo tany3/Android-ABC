337865d7d41cd0581c8eec1eab5bff4c3719fa8e
mtaniuchi <mtaniuchi@gmail.com>
2018-07-01 12:06:43 +0900

    modified: 時刻表示画面の作成

--- 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/MainActivity.kt
+++ 01_WorldClock/app/src/main/java/net/tiesfeed/android/sample/worldclock/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 画面のレイアウトを指定
         setContentView(R.layout.activity_main)
     }
 }
