ab133f21ea1b5ea7a2ae915d24387a235db985cf
Masahiro Taniuchi <taniuchi@sansan.com>
2018-08-14 18:32:11 +0900

    modified ArticleViewの表示を確認する

--- 02_QiitaClient/app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ 02_QiitaClient/app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -2,11 +2,25 @@ package sample.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import sample.qiitaclient.model.Article
+import sample.qiitaclient.model.User
+import sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ArticleViewオブジェクトを生成
+        val articleView = ArticleView(applicationContext)
+
+        // Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
+        articleView.setArticle(Article("123",
+                "Kotlin入門",
+                "http://www.example.com/article/123",
+                User("456", "たろう", "")))
+
+        setContentView(articleView)
+        //setContentView(R.layout.activity_main)
     }
 }
