2383f87e2338867bf0f2abf46b5444507689ca7c
Masahiro Taniuchi <taniuchi@sansan.com>
2018-08-14 16:49:49 +0900

    added data class

---
+++ 02_QiitaClient/app/src/main/java/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,9 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by tany3 on 2018/08/14.
+ */
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ 02_QiitaClient/app/src/main/java/sample/qiitaclient/model/User.kt
@@ -0,0 +1,8 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by tany3 on 2018/08/14.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
