629a2f7b107fa96b1ed17c333277911a71889e7c
Masahiro Taniuchi <taniuchi@sansan.com>
2018-09-04 15:17:21 +0900

    refactor キャストしなくていい

--- 02_QiitaClient/app/src/main/java/sample/qiitaclient/extensions.kt
+++ 02_QiitaClient/app/src/main/java/sample/qiitaclient/extensions.kt
@@ -7,5 +7,5 @@ import android.view.View
  * Created by tany3 on 2018/09/04.
  */
 fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
-    findViewById<T>(id) as T
+    findViewById<T>(id)
 }
