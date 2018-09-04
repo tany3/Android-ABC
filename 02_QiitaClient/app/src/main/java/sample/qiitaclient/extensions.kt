9aea7d65b46d7a307fcf8c3ef00378d32ef26985
Masahiro Taniuchi <taniuchi@sansan.com>
2018-09-04 14:33:17 +0900

    add Viewをvalでnon-nullな遅延初期化するプロパティに変更ためのextensionを追加

---
+++ 02_QiitaClient/app/src/main/java/sample/qiitaclient/extensions.kt
@@ -0,0 +1,11 @@
+package sample.qiitaclient
+
+import android.support.annotation.IdRes
+import android.view.View
+
+/**
+ * Created by tany3 on 2018/09/04.
+ */
+fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
+    findViewById<T>(id) as T
+}
