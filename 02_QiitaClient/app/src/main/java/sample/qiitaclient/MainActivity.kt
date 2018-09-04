680eb068fe403b05465ab97379b8984ada6bab66
Masahiro Taniuchi <taniuchi@sansan.com>
2018-09-04 15:18:15 +0900

    add 記事のリスト表示

--- 02_QiitaClient/app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ 02_QiitaClient/app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -1,26 +1,29 @@
 package sample.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
-import sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // ArticleViewオブジェクトを生成
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(
+                dummyArticle("Kotlin入門", "たろう"),
+                dummyArticle("Java入門", "じろう"))
 
-        // Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
-        articleView.setArticle(Article("123",
-                "Kotlin入門",
-                "http://www.example.com/article/123",
-                User("456", "たろう", "")))
-
-        setContentView(articleView)
-        //setContentView(R.layout.activity_main)
+        val listView: ListView = findViewById(R.id.list_view)
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https:/kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
